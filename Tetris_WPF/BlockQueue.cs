4ddd344b4f6a32174f3efd83066ef2d1f3cc080e
redfoxbg <redfoxbg@gmail.com>
2023-08-28 00:31:30 +0300

    Adding Blocks for game and block queue

---
+++ Tetris_WPF/BlockQueue.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Tetris_WPF.Blocks;
+
+namespace Tetris_WPF
+{
+    public class BlockQueue
+    {
+        private readonly Random random = new Random();
+
+        public BlockQueue()
+        {
+            NextBlock = RandomBlock();
+        }
+
+        private readonly Block[] blocks = new Block[]
+        {
+            new IBlock(),
+            new JBlock(),
+            new LBlock(),
+            new OBlock(),
+            new SBlock(),
+            new TBlock(),
+            new ZBlock()
+        };
+
+        public Block NextBlock { get; private set; }
+
+        private Block RandomBlock() => blocks[random.Next(blocks.Length)];
+
+        public Block GetAndUpdate()
+        {
+            Block block = NextBlock;
+
+            do
+            {
+                NextBlock = RandomBlock();
+            } while (block.Id == NextBlock.Id);
+
+            return block;
+        }
+    }
+}
