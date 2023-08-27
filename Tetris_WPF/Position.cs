474798a989b568ae7cffd9676fbebc0f1fff19e0
redfoxbg <redfoxbg@gmail.com>
2023-08-27 22:17:50 +0300

    Adding Block and Position classes

--- Tetris_WPF/Position.cs
+++ Tetris_WPF/Position.cs
@@ -1,12 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Tetris_WPF
+﻿namespace Tetris_WPF
 {
     public class Position
     {
+        public Position(int row, int column)
+        {
+            Row = row;
+            Column = column;
+        }
+
+        public int Row { get; set; }
+
+        public int Column { get; set; }
     }
 }
