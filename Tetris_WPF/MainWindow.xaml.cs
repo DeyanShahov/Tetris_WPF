3849fce3c074be8401b9cdbe9a2c5da0afef2933
redfoxbg <redfoxbg@gmail.com>
2023-08-28 14:22:25 +0300

    Adding speed on blocks

--- Tetris_WPF/MainWindow.xaml.cs
+++ Tetris_WPF/MainWindow.xaml.cs
@@ -45,6 +45,10 @@ namespace Tetris_WPF
         };
 
         private readonly Image[,] imageControls;
+        private readonly int maxDelay = 1000;
+        private readonly int minDelay = 75;
+        private readonly int delayDecrease = 25;
+
 
         private GameState gameState = new GameState();
 
@@ -141,7 +145,8 @@ namespace Tetris_WPF
 
             while (!gameState.GameOver)
             {
-                await Task.Delay(500);
+                int delay = Math.Max(minDelay, maxDelay - (gameState.Score * delayDecrease));
+                await Task.Delay(delay);
                 gameState.MoveBlockDown();
                 Draw(gameState);
             }
